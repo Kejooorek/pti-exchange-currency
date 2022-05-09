83486f83a5b546e1dd22f4628d1748117e2db358
Robcio <Robcio255@Onet.pl>
2022-05-09 21:31:15 +0200

    feat: prepared api and component

--- pages/exchange_page/index.tsx
+++ pages/exchange_page/index.tsx
@@ -10,43 +10,17 @@ import useSWR from "swr";
 const Curriencies = () => {
   const { data: session, status } = useSession();
   const [wallet, setWallet] = useState([]);
+  const [amount, setAmount] = useState(0);
   const { data, error } = useSWR<APIResponse>("/api/get_codes", fetcher);
   const [fromCurrencyCode, setFromCurrencyCode] = useState("");
   const [toCurrencyCode, setToCurrencyCode] = useState("");
-  const getCurrencyExchange = async () => {
+  async function getCurrencyExchange() {
     await axios.post(`/api/exchange/${fromCurrencyCode}/${toCurrencyCode}`);
-  };
+  }
 
   return (
     <>
-      <Navbar></Navbar>value
-      <input type="text" />
-      {/* <CreditCard name={session!.user!.name}></CreditCard> */}
-      <h1>From</h1>
-      <select
-        name=""
-        id=""
-        onChange={(e) => setFromCurrencyCode(e.target.value)}
-      >
-        {data?.res.map((d) => {
-          return (
-            <option value={d[0]}>
-              {d[0]} - {d[1]}
-            </option>
-          );
-        })}
-      </select>
-      <h1>To</h1>
-      <select name="" id="" onChange={(e) => setToCurrencyCode(e.target.value)}>
-        {data?.res.map((d) => {
-          return (
-            <option value={d[0]}>
-              {d[0]} - {d[1]}
-            </option>
-          );
-        })}
-      </select>
-      <button onClick={() => getCurrencyExchange()}>SIU</button>
+      <Navbar></Navbar>
     </>
   );
 };
