00e22a23b5edba7d9b4129cd50801c141a14e46e
Robcio <Robcio255@Onet.pl>
2022-05-10 21:29:52 +0200

    feat: made simple mockup of currency converter... still in progress

--- pages/exchange_page/index.tsx
+++ pages/exchange_page/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import CreditCard from "../../components/CreditCard";
 import EuropaMap from "../../components/EuropaMap";
 import Navbar from "../../components/Navbar";
@@ -11,25 +11,36 @@ const Curriencies = () => {
   const { data: session, status } = useSession();
   const [wallet, setWallet] = useState([]);
   const [amount, setAmount] = useState(0);
-  const [currencyCodes, setCurrencyCodes] = useState([])
-  const { data, error } = useSWR<APIResponse>("/api/get_codes", fetcher);
+  const [updatedValue, setUpdatedValue] = useState(0);
+  const [currencyCodes, setCurrencyCodes] = useState([]);
   const [fromCurrencyCode, setFromCurrencyCode] = useState("");
   const [toCurrencyCode, setToCurrencyCode] = useState("");
   async function getCurrencyExchange() {
-    await axios.post(`/api/exchange/${fromCurrencyCode}/${toCurrencyCode}`).then(res => {
-      console.log(res.data.value * amount)
-    })
-    console.log(fromCurrencyCode, toCurrencyCode, amount)
+    await axios
+      .post(`/api/exchange/${fromCurrencyCode}/${toCurrencyCode}`)
+      .then((res) => {
+        // console.log(res.data.value);
+        if (amount > 0) {
+          setUpdatedValue(res.data.value * amount);
+        } else {
+          alert(
+            "Nie mozesz przewalutowac wartosci mniejszej badz rownej zero."
+          );
+        }
+      });
+    setAmount(0);
+
+    console.log(fromCurrencyCode, toCurrencyCode, amount);
   }
 
-  useEffect(() => {
-    fetcher('api/get_codes').then(response => {
-      setCurrencyCodes(response.res)
-    })
-    fetcher('/api/wallet').then(response => {
-      setWallet(response.wallet)
-    })
-  }, [])
+  useMemo(() => {
+    fetcher("api/get_codes").then((response) => {
+      setCurrencyCodes(response.res);
+    });
+    fetcher("/api/wallet").then((response) => {
+      setWallet(response.wallet);
+    });
+  }, []);
 
   // console.log(currencyCodes)/
   // console.table(wallet)
@@ -37,25 +48,42 @@ const Curriencies = () => {
   return (
     <>
       <Navbar></Navbar>
-      <div style={{ height: "100vh" }} className="flex w-full justify-center items-center flex-col">
-
+      <div
+        style={{ height: "100vh" }}
+        className="flex w-full justify-center items-center flex-col"
+      >
         <div className="flex-row">
-          <select className="m-4" name="" id="" onChange={(e) => setFromCurrencyCode(e.target.value)}>
-            {wallet.map(w => {
-              return <option value={w.id}>{w.id}</option>
+          <select
+            className="m-4"
+            name=""
+            id=""
+            onChange={(e) => setFromCurrencyCode(e.target.value)}
+          >
+            {wallet.map((w) => {
+              return <option value={w.id}>{w.id}</option>;
             })}
           </select>
-          <input value={amount} type="number" onChange={(e) => setAmount(parseInt(e.target.value))} />
-
+          <input
+            value={amount}
+            type="number"
+            onChange={(e) => setAmount(parseInt(e.target.value))}
+          />
         </div>
-        <select className="m-4" name="" id="" onChange={(e) => setToCurrencyCode(e.target.value)}>
-          {currencyCodes.map(currencyCode => {
-            return <option value={currencyCode[0]}>{currencyCode[0]}</option>
+        <select
+          className="m-4"
+          name=""
+          id=""
+          onChange={(e) => setToCurrencyCode(e.target.value)}
+        >
+          {currencyCodes.map((currencyCode) => {
+            return <option value={currencyCode[0]}>{currencyCode[0]}</option>;
           })}
         </select>
         <button onClick={() => getCurrencyExchange()}>make req!</button>
+        <p>
+          Przewalutowana wartosc:{updatedValue} {toCurrencyCode}
+        </p>
       </div>
-
     </>
   );
 };
